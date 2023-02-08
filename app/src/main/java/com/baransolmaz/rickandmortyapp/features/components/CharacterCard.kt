9b1826ce03863c3e9d1d69223a9b0409dd705195
baransolmaz <baransolmaz@hotmail.com>
2023-02-08 12:26:45 +0300

    Character Card Updated

--- app/src/main/java/com/baransolmaz/rickandmortyapp/features/components/CharacterCard.kt
+++ app/src/main/java/com/baransolmaz/rickandmortyapp/features/components/CharacterCard.kt
@@ -1,88 +1,178 @@
 package com.baransolmaz.rickandmortyapp.features.components
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.baransolmaz.rickandmortyapp.R
-import com.baransolmaz.rickandmortyapp.data.Character
-import com.baransolmaz.rickandmortyapp.data.SingleCharacterResponse
+import com.baransolmaz.rickandmortyapp.data.response.SingleCharacterResponse
+import com.baransolmaz.rickandmortyapp.models.MainViewModel
+import kotlinx.coroutines.delay
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun CharacterCard(painter: Painter,character:SingleCharacterResponse) {
+fun CharacterCard(
+    painter: Painter = painterResource(id = R.drawable.rick_and_morty),
+    character: SingleCharacterResponse,
+    id:Int=0,
+    navController: NavController?,
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .border(3.dp, Color.Black, RoundedCornerShape(15.dp)),
+            .height(120.dp)
+            .border(3.dp, Color.Black, RoundedCornerShape(15.dp))
+            .clickable {
+                navController!!.navigate(
+                    "details/${character.id}"
+                )
+            },
         shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
+        elevation = 5.dp,
+        backgroundColor = Color.LightGray
     ) {
         Row (
-
             horizontalArrangement = Arrangement.spacedBy(5.dp),
-            verticalAlignment = Alignment.CenterVertically,
+            verticalAlignment = CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.LightGray)
                 .padding(horizontal = 8.dp, vertical = 2.dp)
         ){
             Image(
-                painter =painter,
-                contentDescription ="X",
-                modifier=Modifier.background(Color.Transparent)
+                painter= rememberImagePainter(character.image),
+                contentDescription =character.name,
+                contentScale = ContentScale.Crop,
+                modifier= Modifier
+                    .background(Color.Transparent)
+                    .height(100.dp)
+                    .width(100.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.LightGray)
+                    .border(width = 1.dp, color = Color.Black, RoundedCornerShape(16.dp))
             )
             Column(
-                modifier = Modifier.fillMaxWidth().height(painter.intrinsicSize.height.dp/3),
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(painter.intrinsicSize.height.dp / 3)
+                    .padding(vertical = 5.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                Text(text = character.name, fontSize = 22.sp)
-                Text(text = character.species, fontSize = 18.sp, color = Color.DarkGray)
                 Row(
-                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "${id+1}. ${character.name!!}",
+                        fontSize = 20.sp,
+                        color=Color.Black,
+                    )
+                }
+                Text(text = character.species!!, fontSize = 16.sp, color = Color.DarkGray)
+                Row(
+                    verticalAlignment = CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(5.dp)
 
                 ) {
-                    DeadCircle(character.status)
+                    character.status?.let { DeadCircle(it) }
+                    character.status?.let {
+                        Text(
+                            text = it,
+                            fontSize = 16.sp,
+                            color = Color.DarkGray
+                        )
+                    }
+                    Spacer(modifier = Modifier.size(20.dp))
                     Text(
-                        text = character.status,
-                        fontSize = 18.sp,
+                        text = character.episode.size.toString(),
+                        fontSize = 16.sp,
                         color = Color.DarkGray
                     )
                 }
             }
+            var isClick by remember { mutableStateOf(false) }
+            LaunchedEffect(isClick) {
+                if (isClick) {
+                    delay(800)
+                    isClick = false
+                }
+            }
+            IconButton(
+                onClick = {
+                    isClick=true
+                    character.isLiked = !character.isLiked
+                }
+            ) {
+                val color = if (character.isLiked) Color.Red else Color.Gray
+
+                Icon(
+                    modifier = Modifier
+                        .align(CenterVertically)
+                        .fillMaxWidth()
+                        .height(painter.intrinsicSize.height.dp / 4)
+                        .size(if (isClick) getAnimateFloat().value.dp else 24.dp),
+                    painter = rememberVectorPainter(Icons.Default.Favorite),
+                    contentDescription = null,
+                    tint = color
+                )
+            }
+
         }
     }
 
 }
-@Preview
 @Composable
-private fun BodyPreview() {
-    CharacterCard(
-        painter = painterResource(id = R.drawable.rick_and_morty),
-        SingleCharacterResponse(name = "Rick Sanchez", status = "Alive", species = "Human")
+fun getAnimateFloat(): State<Float> {
+    val infiniteTransition = rememberInfiniteTransition()
+    return infiniteTransition.animateFloat(
+        initialValue = 24.0f,
+        targetValue = 48.0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 800,
+                delayMillis = 100,
+                easing = FastOutLinearInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
     )
 }
 @Composable
 fun DeadCircle(isAlive:String="Alive"){
     Canvas(modifier = Modifier.size(9.dp), onDraw = {
-        drawCircle(color = if (isAlive=="Alive")
-                                Color.Green
-                            else
-                                Color.Red
+        drawCircle(color = when (isAlive) {
+                                "Alive" -> Color.Green
+                                "Dead" -> Color.Red
+                                else -> Color.Gray
+                            }
                 )
     })
+}
+
+@Preview
+@Composable
+fun Prev() {
+    CharacterCard(character = SingleCharacterResponse(), navController = null)
 }
